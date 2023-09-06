3d451530dcc3065b2bc4b3a0892140e232039f18
taufik69 <taufik35-2915@diu.edu.bd>
2023-09-07 03:30:07 +0600

    chdir problem

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const { argv, command } = require("yargs");
 const { log } = require("console");
 (async () => {
   try {
-    // let pathName = path.resolve(__dirname);
     // fs.readdir(pathName, (err, files) => {
     //   if (err) {
     //     console.log("Cant read file ->", err);
@@ -15,39 +14,42 @@ const { log } = require("console");
     //     });
     //   }
     // });
-    const folderName = ["js", "css"];
-    for (let i = 0; i < folderName.length; i++) {
-      fs.mkdir(folderName[i], (err) => {
-        if (err) {
-          console.error("Error creating folder:", err);
-        } else {
-          console.log(
-            `Folder "${folderName[i]}" has been created successfully.`
-          );
-        }
-      });
-    }
+    const folderName = "js";
 
-    const fileName = "index.html"; // Change this to the name of the folder you want to create
-    const { _: command } = argv;
-    const htmlScaffholding = `<!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
-    </head>
-    <body>
-        
-    </body>
-    </html>`;
-    fs.writeFile(fileName, htmlScaffholding, (err) => {
+     fs.mkdir(folderName, (err) => {
       if (err) {
         console.error("Error creating folder:", err);
+        return;
       } else {
-        console.log(`fileName "${fileName}" has been created successfully.`);
+        console.log(`Folder "${folderName}" has been created successfully.`);
       }
-    });
+    })
+    
+
+    // Change working directory to the main folder
+    process.chdir(folderName);
+    console.log("dir :", path.resolve(__dirname));
+
+    // const fileName = "index.html"; // Change this to the name of the folder you want to create
+    // // const { _: command } = argv;
+    // const htmlScaffholding = `<!DOCTYPE html>
+    // <html lang="en">
+    // <head>
+    //     <meta charset="UTF-8">
+    //     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    //     <title>Document</title>
+    // </head>
+    // <body>
+
+    // </body>
+    // </html>`;
+    // fs.writeFile(fileName, htmlScaffholding, (err) => {
+    //   if (err) {
+    //     console.error("Error creating folder:", err);
+    //   } else {
+    //     console.log(`fileName "${fileName}" has been created successfully.`);
+    //   }
+    // });
   } catch (error) {
     console.log("error :", error);
   }
